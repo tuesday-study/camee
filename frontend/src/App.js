45d83004172691cb78daf34ca2d49273fc422a20
AhseoKoo <ahseo999@gmail.com>
2019-08-14 17:31:45 +0900

    Add App.js for render pages

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+// 화면 Render하는 곳, app안에서 pages들 render
+
+import React, { Component } from 'react';
+import './style/components_style.css';
+
+export default class App extends Component {
+  render() {
+    return <div className="app" />;
+  }
+}
