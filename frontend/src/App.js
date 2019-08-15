920c369379f1bbe786f4835baaceb0ea7c07d0f3
AhseoKoo <ahseo999@gmail.com>
2019-08-15 16:54:40 +0900

    Add Route and return Footer to change pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 // 화면 Render하는 곳, app안에서 pages들 render
 
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './style/components_style.css';
+import Footer from './components/Footer';
+import Approval from './pages/Approval';
+import Chat from './pages/Chat';
+import Group from './pages/Group';
+import LookUp from './pages/LookUp';
+import MyPage from './pages/MyPage';
 
 export default class App extends Component {
   render() {
-    return <div className="app" />;
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <div>
+            <Footer />
+            <Route exact path="/" component={LookUp} />
+            <Route path="/approval" component={Approval} />
+            <Route path="/group" component={Group} />
+            <Route path="/Chat" component={Chat} />
+            <Route path="/MyPage" component={MyPage} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
