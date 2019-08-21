23d7caa01adc94c945164d2d4204ac0c694efb3d
AhseoKoo <ahseo999@gmail.com>
2019-08-21 14:06:00 +0900

    Add buttons by material-ui

--- frontend/src/pages/Group.js
+++ frontend/src/pages/Group.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import MakeGroup from '../components/MakeGroup';
 import '../style/pages_style.css';
 
 // axios 통해 back에서  Group Profile data 가져오기
 
 class group extends Component {
   render() {
-    return <div>GroupProfile</div>;
+    return <MakeGroup />;
   }
 }
 export default group;
