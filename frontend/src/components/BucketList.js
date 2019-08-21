183c821e392c0227eef04af5d4177eff3dfa3d31
AhseoKoo <ahseo999@gmail.com>
2019-08-21 14:03:50 +0900

    Add group view form in bucketlist and likefrom

--- frontend/src/components/BucketList.js
+++ frontend/src/components/BucketList.js
@@ -1,7 +1,27 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import '../style/components_style.css';
 
 export default class BucketList extends Component {
   render() {
-    return 'Bucket List of your Group';
+    return (
+      <div className="grouplist">
+        <div className="grouplist_photo middle">Photo</div>
+        <div className="grouplist_introduce">
+          Introduce
+          <br />
+          Hi
+        </div>
+        <div className="bucketlist_button middle">
+          <Button variant="contained" color="primary">
+            LIKE
+          </Button>
+          <br />
+          <Button variant="contained" color="default">
+            추천
+          </Button>
+        </div>
+      </div>
+    );
   }
 }
