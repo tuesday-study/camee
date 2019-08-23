6f47568b2f5e7172074a0fb79143455425f1c2a1
AhseoKoo <ahseo999@gmail.com>
2019-08-23 16:30:36 +0900

    Add provider to use Middleware

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
 // import { Provider } from 'react-redux';
 // import { createStore, applyMiddleware } from 'redux';
 // import reduxThunk from 'redux-thunk';
 
 import App from './App';
+import reducers from './reducers';
 // import reducers from './reducers';
 
 // const store = createStore({}, {}, applyMiddleware(reduxThunk));
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
