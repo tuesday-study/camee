6c4c12023970ce03d9363497f4adb0c529d76967
AhseoKoo <ahseo999@gmail.com>
2019-08-23 16:28:41 +0900

    Remove BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,18 @@
 // 화면 Render하는 곳, app안에서 pages들 render
 
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import './style/components_style.css';
 import Footer from './components/Footer';
-import Approval from './pages/Approval';
-import Chat from './pages/Chat';
-import Group from './pages/Group';
-import LookUp from './pages/LookUp';
-import MyPage from './pages/MyPage';
-import ChatPage from './components/ChatPage';
+import RenderPages from './components/RenderPages';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <BrowserRouter>
-          <Footer />
-          <div className="pages">
-            <Route exact path="/" component={LookUp} />
-            <Route path="/Approval" component={Approval} />
-            <Route path="/Group" component={Group} />
-            <Route exact path="/Chat" component={Chat} />
-            <Route path="/Chat/ChatPage" component={ChatPage} />
-            <Route path="/MyPage" component={MyPage} />
-          </div>
-        </BrowserRouter>
+        <Footer />
+        <div className="pages">
+          <RenderPages />
+        </div>
       </div>
     );
   }
