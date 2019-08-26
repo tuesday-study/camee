7679d26e97fbc3c9e927db99a05cbef82d0b2c99
AhseoKoo <ahseo999@gmail.com>
2019-08-26 16:24:06 +0900

    Change the Approval component by material-ui

--- frontend/src/pages/Approval.js
+++ frontend/src/pages/Approval.js
@@ -1,35 +1,79 @@
-import React, { Component } from 'react';
-import BucketList from '../components/BucketList';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import LikeFrom from '../components/LikeFrom';
-import '../style/pages_style.css';
+import BucketList from '../components/BucketList';
 
-// axios 통해 back에서 우리 group에 대한 bucket, like from data 가져오기
-// 각 Tab에서 Component에 있는 Bucket 혹은 LikeFrom .js 불러와서 호출
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-class approval extends Component {
-  render() {
-    return [
-      <ul className="nav nav-tabs">
-        <li className="nav-item tab-item">
-          <a className="nav-link active" data-toggle="tab" href="#like">
-            LIKE FROM
-          </a>
-        </li>
-        <li className="nav-item tab-item">
-          <a className="nav-link" data-toggle="tab" href="#bucket">
-            BUCKET
-          </a>
-        </li>
-      </ul>,
-      <div className="tab-content">
-        <div className="tab-pane show active" id="like">
-          <LikeFrom />
-        </div>
-        <div className="tab-pane" id="bucket">
-          <BucketList />
-        </div>
-      </div>
-    ];
-  }
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      <Box p={2}>{children}</Box>
+    </Typography>
+  );
 }
-export default approval;
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: 'beige',
+    width: 375
+  }
+}));
+
+export default function FullWidthTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="LikeFrom" {...a11yProps(0)} />
+          <Tab label="Bucket" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <LikeFrom />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <BucketList />
+      </TabPanel>
+    </div>
+  );
+}
