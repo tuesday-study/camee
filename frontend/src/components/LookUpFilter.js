86d0f2caa02ad21cb62ce5bb1b7010b635e3333f
AhseoKoo <ahseo999@gmail.com>
2019-08-26 16:28:18 +0900

    Modify the filter button by using material ui

--- frontend/src/components/LookUpFilter.js
+++ frontend/src/components/LookUpFilter.js
@@ -1,58 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-export default class LookUpFilter extends Component {
-  render() {
-    return (
-      <div className="filterbutton">
-        <button
-          type="button"
-          className="btn btn-danger"
-          data-toggle="modal"
-          data-target="#FilterModal"
-        >
-          Filter
-        </button>
-
-        <div
-          className="modal"
-          id="FilterModal"
-          tabIndex="-1"
-          role="dialog"
-          aria-labelledby="FilterModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="FiltereModalLabel">
-                  Filter
-                </h5>
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">Select Your Filter</div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-dismiss="modal"
-                >
-                  Close
-                </button>
-                <button type="button" className="btn btn-danger">
-                  Save changes
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+const useStyles = makeStyles(theme => ({
+  typography: {
+    padding: theme.spacing(2)
   }
+}));
+
+export default function LookUpFilter() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  return (
+    <div className="filterbutton">
+      <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+        FILTER
+      </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+      >
+        <Typography className={classes.typography}>Filtering...</Typography>
+      </Popover>
+    </div>
+  );
 }
