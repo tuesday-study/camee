6cb57af2b9bdc4443937186b0f1697288d4b4de1
AhseoKoo <ahseo999@gmail.com>
2019-08-27 15:23:29 +0900

    Modify the style of Buttons by Material-ui

--- frontend/src/components/BucketList.js
+++ frontend/src/components/BucketList.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Divider from '@material-ui/core/Divider';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import Button from '@material-ui/core/Button';
 import '../style/components_style.css';
 
-export default class BucketList extends Component {
+const styles = {
+  root: {
+    padding: '0 10 0 10 !important',
+    minWidth: '50px !important'
+  }
+};
+
+class BucketList extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <div className="approvallist">
         <div className="approvallist_photo middle">Photo</div>
@@ -13,15 +25,28 @@ export default class BucketList extends Component {
           Hi
         </div>
         <div className="bucketlist_button middle">
-          <Button variant="contained" color="primary">
-            LIKE
+          <Button
+            variant="text"
+            color="primary"
+            classes={{
+              root: classes.root
+            }}
+          >
+            <FavoriteIcon />
           </Button>
-          <br />
-          <Button variant="contained" color="default">
-            추천
+          <Divider />
+          <Button
+            variant="text"
+            color="default"
+            classes={{
+              root: classes.root
+            }}
+          >
+            <ThumbUpIcon />
           </Button>
         </div>
       </div>
     );
   }
 }
+export default withStyles(styles)(BucketList);
