3d2e88fe1f0bef84db2bf9958ee181f8b65b3089
AhseoKoo <ahseo999@gmail.com>
2019-08-27 15:24:30 +0900

    Add Chatting Page

--- frontend/src/components/ChatPage.js
+++ frontend/src/components/ChatPage.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
+import '../style/pages_style.css';
+import '../style/components_style.css';
 
 class ChatPage extends Component {
   render() {
     return [
-      <div>
-        <Link to="/">
+      <div className="back_button">
+        <Link to="/chat">
           <Button variant="contained" color="DEFAULT">
             BACK
           </Button>
         </Link>
       </div>,
-      <br />,
-      <div>HI</div>
+      <Divider />,
+      <div className="chat_page">HI</div>,
+      <Divider />,
+      <form
+        // onSubmit={this.onFormSubmit}
+        className="input-group input_form"
+      >
+        <input
+          placeholder="Send Your Message"
+          className="standard"
+          //  value={this.state.term}
+          //  onChange={this.onInputChange}
+        />
+        <span className="input-group-btn input_button">
+          <Button variant="contained" color="primary">
+            Send
+          </Button>
+        </span>
+      </form>
     ];
   }
 }
