0a30a3a2247f5e7f76ad9f464bf8963d8d1198aa
AhseoKoo <ahseo999@gmail.com>
2019-08-27 15:18:14 +0900

    Add Router for all components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,28 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './style/components_style.css';
 import Footer from './components/Footer';
-import RenderPages from './components/RenderPages';
+import Approval from './pages/Approval';
+import Chat from './pages/Chat';
+import Group from './pages/Group';
+import LookUp from './pages/LookUp';
+import MyPage from './pages/MyPage';
 import ChatPage from './components/ChatPage';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <Footer />
-        <div className="pages">
-          <BrowserRouter>
-            <Route exact path="/" component={RenderPages} />
-            <Route path="/ChatPage" component={ChatPage} />
-          </BrowserRouter>
-        </div>
+        <BrowserRouter>
+          <Footer />
+          <div className="pages">
+            <Route exact path="/" component={LookUp} />
+            <Route path="/approval" component={Approval} />
+            <Route exact path="/chat" component={Chat} />
+            <Route path="/group" component={Group} />
+            <Route path="/mypage" component={MyPage} />
+            <Route path="/Chat/ChatPage" component={ChatPage} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
