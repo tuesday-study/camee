aa6ed836344444544bad186deb14aca913002bbd
AhseoKoo <ahseo999@gmail.com>
2019-08-27 15:45:20 +0900

    Modify function to class component

--- frontend/src/pages/Approval.js
+++ frontend/src/pages/Approval.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -39,41 +39,49 @@ function a11yProps(index) {
   };
 }
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: 'beige',
-    width: 375
+export default class FullWidthTabs extends Component {
+  useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+      backgroundColor: 'beige',
+      width: 375
+    }
+  }));
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
-}));
 
-export default function FullWidthTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  function handleChange(event, newValue) {
-    setValue(newValue);
+  handleChange(event, newValue) {
+    this.setState({ value: newValue });
   }
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="LikeFrom" {...a11yProps(0)} />
-          <Tab label="Bucket" {...a11yProps(1)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        <LikeFrom />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <BucketList />
-      </TabPanel>
-    </div>
-  );
+  render() {
+    return (
+      <div className={this.useStyles.root}>
+        <AppBar position="static">
+          <Tabs
+            value={this.state.value}
+            onChange={this.handleChange}
+            variant="fullWidth"
+            aria-label="full width tabs example"
+          >
+            <Tab label="LikeFrom" {...a11yProps(0)} />
+            <Tab label="Bucket" {...a11yProps(1)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={this.state.value} index={0}>
+          <LikeFrom />
+        </TabPanel>
+        <TabPanel value={this.state.value} index={1}>
+          <BucketList />
+        </TabPanel>
+      </div>
+    );
+  }
 }
