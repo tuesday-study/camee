442ca861d5128b1b33b1ee1c7463e96185c6568f
AhseoKoo <ahseo999@gmail.com>
2019-08-28 14:54:36 +0900

    Change the represent image style(add border) and change the photoes

--- frontend/src/components/MyProfilePhoto.js
+++ frontend/src/components/MyProfilePhoto.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import Img1 from './emma-watson2-22jun17-pa.jpg';
-import Img2 from './girlfriend.jpg';
-import Img3 from './girlfriend_2.jpeg';
-import Img4 from './girlfriend_3.jpg';
+import '../style/components_style.css';
+import Img1 from './girlfriend_1.jpeg';
+import Img2 from './girlfriend_2.jpeg';
+import Img3 from './girlfriend_3.jpg';
+import Img4 from './girlfriend_4.jpg';
 
 const styles = {
   root: {
@@ -28,17 +29,16 @@ class MyProfilePhoto extends Component {
       <div className={classes.root}>
         <GridList cellHeight={156} className={classes.gridList} cols={2}>
           <GridListTile cols={1}>
-            <img src={Img1} border="5" alt="Emma" />
+            <img src={Img1} alt="girlfriend1" className="represent_image" />
           </GridListTile>
           <GridListTile cols={1}>
-            {' '}
-            <img src={Img2} alt="girlfriend1" />
+            <img src={Img2} alt="girlfriend2" />
           </GridListTile>
           <GridListTile cols={1}>
-            <img src={Img3} alt="girlfriend2" />
+            <img src={Img3} alt="girlfriend3" />
           </GridListTile>
           <GridListTile cols={1}>
-            <img src={Img4} alt="girlfriend3" />
+            <img src={Img4} alt="girlfriend4" />
           </GridListTile>
         </GridList>
       </div>
