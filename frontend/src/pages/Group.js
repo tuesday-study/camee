80893cb0a10673739de0ff70c909524c8ec4d2d4
AhseoKoo <ahseo999@gmail.com>
2019-08-28 20:44:41 +0900

    Change import code(MakeGroup -> NoGroup)

--- frontend/src/pages/Group.js
+++ frontend/src/pages/Group.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import MakeGroup from '../components/MakeGroup';
+import NoGroup from '../components/NoGroup';
 import '../style/pages_style.css';
 
 // axios 통해 back에서  Group Profile data 가져오기
 
 class Group extends Component {
   render() {
-    return <MakeGroup />;
+    return <NoGroup />;
   }
 }
 export default Group;
